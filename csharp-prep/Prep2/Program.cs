4716c3ab07ecb7484ab5540041a5ae66685a1f94
bgardner-edu <gar19078@byui.edu>
2024-09-17 20:17:08 -0600

    W01 Prove prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int input = int.Parse(Console.ReadLine());
+        string grade = GetLetterGrade(input);
+        Console.WriteLine($"Letter Grade: {grade}");
+        if (input > 70)
+        {
+            Console.WriteLine("Congradulations you've passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+        
+    }
+    private static string GetLetterGrade(float percentage)
+    {
+        if (percentage >= 90)
+        {
+            return "A";
+        }
+        else if (percentage >= 80)
+        {
+            return "B";
+        }
+        else if (percentage >= 70)
+        {
+            return "C";
+        }
+        else if (percentage >= 60)
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
     }
 }
