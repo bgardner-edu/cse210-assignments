7ae07b0c48e31469e4d6cec62773beb6e344b2c9
bgardner-edu <gar19078@byui.edu>
2024-09-19 22:15:30 -0600

    W02 Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 11);
+        int count = 1;
+        int numOfTries = 10;
+
+        Console.WriteLine("Guess what number");
+        var guessNumber = int.Parse(Console.ReadLine());
+
+        while (randomNumber != guessNumber && count < numOfTries)
+        {
+            if (randomNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+            count++;
+        }
+        if (count < 10)
+        {
+            Console.WriteLine($"You are correct! It took you {count} tries.");
+        }
+        else
+        {
+            Console.WriteLine("You ran out of tries...");
+        }
+        
     }
 }
