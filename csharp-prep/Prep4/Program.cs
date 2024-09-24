1bcee02b3119e1348d643c6cf571cea78a133fe2
bgardner-edu <gar19078@byui.edu>
2024-09-23 21:03:47 -0600

    W02 Prep 4 Lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do{
+            Console.Write("Enter number:");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+        while (number != 0);
+        Console.WriteLine($"The sum is: {GetSum(numbers)}");
+        Console.WriteLine($"The average is: {GetAverage(numbers)}");
+        Console.WriteLine($"The largest number is: {GetLargest(numbers)}");
+    }
+    public static double GetSum(List<int> numbers)
+    {
+        return numbers.Sum();
+    }
+    public static double GetAverage(List<int> numbers)
+    {
+        return numbers.Average();
+    }
+        public static double GetLargest(List<int> numbers)
+    {
+        return numbers.Max();
     }
 }
