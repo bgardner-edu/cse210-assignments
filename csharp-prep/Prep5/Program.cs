d6267814e2b05739ebe1d79b8bf7dcf6f61cb971
bgardner-edu <gar19078@byui.edu>
2024-09-23 21:15:56 -0600

    W02 Prep 5 functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.IO.IsolatedStorage;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResults(name, square);
+    }
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    public static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    public static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    public static void DisplayResults(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
