6f04d12f7aa98a66b906f6fd856e6c21975dd6a5
bgardner-edu <gar19078@byui.edu>
2024-10-01 21:17:31 -0600

    W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Junior Software engineer";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume resume = new Resume();
+        resume._name = "Bradley Gardner";
+        resume._jobs = [job1, job2];
+        resume.Display();
     }
 }
