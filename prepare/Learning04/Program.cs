f506ed3006f7e2af9a8b386740fdb7bdff1766ba
bgardner-edu <gar19078@byui.edu>
2024-10-29 21:22:51 -0600

    W07 Prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samual Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Reberto Rodriquez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes off World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
