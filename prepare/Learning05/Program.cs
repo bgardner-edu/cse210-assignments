118885c7b8f5ea32999d2fdfe1fe456b15cce81a
bgardner-edu <gar19078@byui.edu>
2024-11-09 21:06:04 -0700

    W08 Prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sqr = new Square(3, "Red");
+        Rectangle rect = new Rectangle(3, 4, "Blue");
+        Circle circle = new Circle(3, "Yellow");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sqr);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
